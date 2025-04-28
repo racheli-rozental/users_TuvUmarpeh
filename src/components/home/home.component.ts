189ea4a8cda5412c00c9ba9523f728bff1e44105
racheli-rozental <racheli74621@gmail.com>
2025-04-28 16:12:01 +0300

    update

--- src/components/home/home.component.ts
+++ src/components/home/home.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { LoginComponent } from "../login/login.component";
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [LoginComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
--- src/components/navigate/navigate.component.ts
+++ src/components/navigate/navigate.component.ts
@@ -1,30 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import {UsersService } from '../../service/users.service';
+import { UsersService } from '../../service/users.service';
 import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-navigate',
   imports: [MatIconModule],
   templateUrl: './navigate.component.html',
-  styleUrl:'./navigate.component.css'
+  styleUrl: './navigate.component.css'
 })
-export class NavigateComponent  {
-constructor(private router: Router, private UserService: UsersService) {}
-  onRegister() {
-    this.router.navigate(['/register']);
+export class NavigateComponent {
+  token=sessionStorage.getItem('token')||'';
+
+  constructor(private router: Router, private userService: UsersService) {
   }
 
-  
-  onLogin() {
-    this.router.navigate(['/login']);
-  }
-  onActivity() {
-    this.router.navigate(['/activity']);
-  }
-  onUpdate(){
-    this.router.navigate(['/update']);
-  }
+
+onRegister() {
+  this.router.navigate(['/register']);
+}
+
+onLogin() {
+  this.router.navigate(['/']);
+}
+onActivity() {
+  this.router.navigate(['/activity']);
+}
+onUpdate(){
+  this.router.navigate(['/update']);
+}
 
  
 }

574442ab334bba0129e093721647e3a8cd99bdb1
racheli-rozental <racheli74621@gmail.com>
2025-06-10 00:36:55 +0300

    refresh

--- src/components/register-for-activity/register-for-activity.component.ts
+++ src/components/register-for-activity/register-for-activity.component.ts
@@ -1,7 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+// import { Component, OnInit } from '@angular/core';
+// import { UsersService } from '../../service/users.service';
+// import { ActivatedRoute, Router } from '@angular/router';
+// import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+
+// @Component({
+//   selector: 'app-register-for-activity',
+//   imports: [ReactiveFormsModule],
+//   templateUrl: './register-for-activity.component.html',
+//   styleUrls: ['./register-for-activity.component.css']
+// })
+// export class RegisterForActivityComponent {
+//   registerActivityForm!: FormGroup;
+
+//   constructor(private userService: UsersService, private route: ActivatedRoute, private router: Router, private fb: FormBuilder) {
+//     this.registerActivityForm = this.fb.group({
+//       IdNumber: ['', Validators.required]
+//     });
+//   }
+
+//   registerForActivity() {
+//     const activityIdString = this.route.snapshot.paramMap.get('id');
+//     const activityId = activityIdString ? Number(activityIdString) : null;
+
+//     if (activityId) {
+//       const idNumber = this.registerActivityForm.value.IdNumber;
+
+//       this.userService.registerActivity(idNumber, activityId).subscribe(response => {
+//         console.log('נרשמת בהצלחה:', response);
+//         alert('נרשמת בהצלחה לפעילות');
+//         this.router.navigate(['/activity']);
+//       }, error => {
+//         console.error('Error enrolling user:', error);
+//         alert(error);
+        
+//       });
+//     }
+//   }
+// }
+///////////////////
+import { Component } from '@angular/core';
 import { UsersService } from '../../service/users.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-register-for-activity',
@@ -12,7 +53,13 @@ import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angula
 export class RegisterForActivityComponent {
   registerActivityForm!: FormGroup;
 
-  constructor(private userService: UsersService, private route: ActivatedRoute, private router: Router, private fb: FormBuilder) {
+  constructor(
+    private userService: UsersService,
+    private route: ActivatedRoute,
+    private router: Router,
+    private fb: FormBuilder,
+    private snackBar: MatSnackBar
+  ) {
     this.registerActivityForm = this.fb.group({
       IdNumber: ['', Validators.required]
     });
@@ -25,13 +72,23 @@ export class RegisterForActivityComponent {
     if (activityId) {
       const idNumber = this.registerActivityForm.value.IdNumber;
 
-      this.userService.registerActivity(idNumber, activityId).subscribe(response => {
-        console.log('User enrolled successfully:', response);
-        this.router.navigate(['/activity']);
-      }, error => {
-        console.error('Error enrolling user:', error);
-        // כאן ניתן להוסיף הודעת שגיאה למשתמש
-      });
+      this.userService.registerActivity(idNumber, activityId).subscribe(
+        response => {
+          console.log('נרשמת בהצלחה:', response);
+          this.snackBar.open('נרשמת בהצלחה לפעילות!', 'סגור', {
+            duration: 4000,
+            panelClass: ['snackbar-success']
+          });
+          this.router.navigate(['/activity']);
+        },
+        error => {
+          console.error('Error enrolling user:', error);
+          this.snackBar.open('אירעה שגיאה בהרשמה לפעילות', 'סגור', {
+            duration: 4000,
+            panelClass: ['snackbar-error']
+          });
+        }
+      );
     }
   }
 }
